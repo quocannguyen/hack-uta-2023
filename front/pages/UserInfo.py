00b7128ecf54fef9c31f6c43811c97abbaf95b30
Wei Yuan Liew <117871652+anemonemonemone@users.noreply.github.com>
2023-10-08 01:48:04 -0500

    Deleted workout.py, updated UserInfo.py

--- front/pages/UserInfo.py
+++ front/pages/UserInfo.py
@@ -1,8 +1,39 @@
 import streamlit as st
 
 #Asks the user about their biological sex, age, weight, and height
-st.header("User info:")
-sex = st.radio("Sex:",["Male", "Female"])
-age = st.number_input("Age:", 1, 110)
-weight = st.number_input("Weight in lbs:", 50,500)
-height = st.number_input("Height in inches:", 4.00,7.00)
+form1 = st.form(key="Info")
+form1.header("User info:")
+sex = form1.radio("Sex:",["Male", "Female"])
+age = form1.number_input("Age:", 1, 110)
+weight = form1.number_input("Weight in kg:", 25,250)
+height = form1.number_input("Height in m:", 0.90,2.50)
+goal = form1.text_input("Your goal with this app: ")
+
+#Calculates the BMI of user and gives appropriate comments
+if form1.form_submit_button("Calculate your BMI here"):
+    BMI = weight/(height*height)
+    st.write("Your BMI value is: ", str(BMI))
+    if (BMI<19):
+        st.write("You are underweight.")
+    elif(BMI >= 19 and BMI <25):
+        st.write("You are healthy. Good job!")
+    elif(BMI >= 25 and BMI <30):
+        st.write("You are overweight.")
+    elif(BMI >= 30 and BMI <40):
+        st.write("You are obese.")
+    else:
+        st.write("You are extremely obese.")
+
+
+    suggestion = st.expander("Expand here to see suggestions.")
+    with suggestion:
+        if (BMI<19):
+            suggestion.write("You are underweight.")
+        elif(BMI >= 19 and BMI <25):
+            suggestion.write("You are healthy. Good job!")
+        elif(BMI >= 25 and BMI <30):
+            suggestion.write("You are overweight.")
+        elif(BMI >= 30 and BMI <40):
+            suggestion.write("You are obese.")
+        else:
+            suggestion.write("You are extremely obese.")
