def16956217592b2cdb391259f6b54a0f3666991
Nishith Guda <68877236+threatdead@users.noreply.github.com>
2023-10-08 04:30:52 -0500

    predictions

--- front/pages/Activity.py
+++ front/pages/Activity.py
@@ -3,29 +3,44 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import joblib
+from sklearn.preprocessing import StandardScaler
+
 
 @st.cache_data
 def load_matlab_data():
     engine = matlab.engine.start_matlab()
     data = engine.load("trial1.mat")
-    
+
     acceleration = data["Acceleration"]
     x = np.array(engine.getfield(acceleration, "X"))
     y = np.array(engine.getfield(acceleration, "Y"))
     z = np.array(engine.getfield(acceleration, "Z"))
 
+    positions = data["Position"]
+    speeds = np.array(engine.getfield(positions, "speed"))
+
     df_X = pd.DataFrame(x)
     df_Y = pd.DataFrame(y)
     df_Z = pd.DataFrame(z)
 
-    return df_X, df_Y, df_Z
+    return df_X, df_Y, df_Z, speeds
+
 
 model = joblib.load("hack_uta_mod.joblib")
-X, Y, Z = load_matlab_data()
-
+X, Y, Z, speed = load_matlab_data()
 df = pd.concat([X, Y, Z], axis=1)
-print(df)
+predictions = model.predict(df)
+index = np.argmax(predictions)
 
-print(model.predict(df))
-#st.title("Activity")
-#st.line_chart(speed)
+prediction = ""
+if predictions[index] == 0:
+    prediction = "Walk"
+elif predictions[index] == 1:
+    prediction = "Climb"
+elif predictions[index] == 2:
+    prediction = "Run"
+
+st.title("Activity")
+st.line_chart(speed)
+
+st.text(prediction)
